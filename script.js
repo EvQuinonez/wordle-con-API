650bc90b63e5d67c1b031900ad88b6c62f1a3e02
Evelyn Quiñonez <quinonezevelyn20@gmail.com>
2024-06-30 23:32:41 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,63 +1,75 @@
-let intentos = 6;
-let diccionario = ['PARED', 'HORNO', 'CALLE', 'APPLE'];
-const palabra = diccionario[Math.floor(Math.random() * diccionario.length)];
+let intentos = 5;
+let palabra;
 
-window.addEventListener('load', init);
+fetch('https://random-word-api.herokuapp.com/word?length=5&lang=es')
+    .then(response => response.json())
+    .then(response => {
+        palabra = response[0].toUpperCase()
+        console.log(palabra)
+    })
+    .catch(err => console.log(err));
 
-function init(){
-    console.log('Esto se ejecuta solo cuando se carga la pagina web')
-    const button = document.getElementById("guess-button");
-    button.addEventListener("click", intentar);
+const button = document.getElementById("intentar");
+button.addEventListener("click", intentar);
 
-    function intentar() {
-        const INTENTO = leerIntento();
-        const GRID = document.getElementById("grid");
-        const ROW = document.createElement('div');
-        ROW.className = 'row';
-        if (INTENTO === palabra) {
-            terminar("<h1>GANASTE!😀</h1>")
-            return
-        }
-        for (let i in palabra) {
-            const SPAN = document.createElement('span');
-            SPAN.className = 'letter';
-            if (INTENTO[i] === palabra[i]) {                //VERDE
-                console.log(INTENTO[i], "VERDE")
-                SPAN.innerHTML = INTENTO[i];
-                SPAN.style.backgroundColor = '#79b851';
-            } else if (palabra.includes(INTENTO[i])) {      //AMARILLO
-                console.log(INTENTO[i], "AMARILLO")
-                SPAN.innerHTML = INTENTO[i];
-                SPAN.style.backgroundColor = '#f3c237';
-            } else {                                        //GRIS
-                console.log(INTENTO[i], "GRIS")
-                SPAN.innerHTML = INTENTO[i];
-                SPAN.style.backgroundColor = 'a4aec4';
-            }
-            ROW.appendChild(SPAN)
-        }
-        GRID.appendChild(ROW)
-        intentos--
-        if (intentos == 0) {
-            terminar("<h1>PERDISTE!😖</h1>")
-        }
+const ayudaButton = document.getElementById("ayuda");
+ayudaButton.addEventListener("click", () => {
+    window.open("https://www.xataka.com/basics/que-wordle-quien-ha-creado-como-se-juega", "_blank");
+});
+
+const input = document.getElementById("entrada");
+input.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+        intentar();
+    }
+});
+
+function intentar() {
+    const INTENTO = leerIntento();
+
+    if (!INTENTO || INTENTO.length !== palabra.length) {
+        alert(`La palabra debe tener ${palabra.length} letras`);
+        return;
     }
 
-    function terminar(mensaje) {
-        const INPUT = document.getElementById("guess-input");
-        const BOTON = document.getElementById("guess-button");
-        INPUT.disabled = true;
-        BOTON.disabled = true;
-        let contenedor = document.getElementById('guesses');
-        contenedor.innerHTML = mensaje;
-    }
+    const GRID = document.getElementById("grid");
+    const ROW = document.createElement('div');
 
-    function leerIntento() {
-        let intento = document.getElementById("guess-input");
-        intento = intento.value;
-        intento = intento.toUpperCase();
-        return intento;
+    ROW.className = 'row';
+    for (let i in palabra) {
+        const SPAN = document.createElement('span');
+        SPAN.className = 'letter';
+        if (INTENTO[i] === palabra[i]) {
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundImage = 'linear-gradient(to right, green, limegreen)';
+        } else if (palabra.includes(INTENTO[i])) {
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundImage = 'linear-gradient(to right, yellow, gold)';
+        } else {
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundImage = 'linear-gradient(to right, grey, darkgrey)';
+        }
+        ROW.appendChild(SPAN);
     }
-    button.addEventListener("click", intentar);
+    GRID.appendChild(ROW);
+    document.getElementById("entrada").value = '';
+    intentos--;
+
+    if (intentos === 0 || INTENTO === palabra) {
+        terminar(INTENTO === palabra ? "<h1>GANASTE!</h1>" : "<h1>PERDISTE!</h1>");
+    }
 }
 
+function leerIntento() {
+    let intento = document.getElementById("entrada").value;
+    intento = intento.toUpperCase();
+    return intento;
+}
+
+function terminar(mensaje) {
+    const input = document.getElementById("entrada");
+    input.disabled = true;
+    button.disabled = true;
+    let contenedor = document.getElementById('guesses');
+    contenedor.innerHTML = mensaje;
+}
